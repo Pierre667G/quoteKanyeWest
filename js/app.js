f43df1cbbae8179ba6f1c94fcc28d4b1ed209abb
Pierre667G <pgerard@edenschool.fr>
2022-04-21 17:12:49 +0200

    add api  in console.log

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ let btn = document.createElement('button')
 btn.textContent = "add quote";
 document.body.appendChild(btn); 
 
+btn.addEventListener('click',()=>{
     fetch('https://api.kanye.rest')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {console.log(data)})
+})
