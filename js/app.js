c9c4b8f245eb1492fff6ff59035a31d4e4ba46e4
Pierre667G <pgerard@edenschool.fr>
2022-04-21 15:28:43 +0200

    add api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 let btn = document.createElement('button')
 btn.textContent = "add quote";
 document.body.appendChild(btn); 
+
+    fetch('https://api.kanye.rest')
+    .then(res => res.json())
+    .then(data => console.log(data))
